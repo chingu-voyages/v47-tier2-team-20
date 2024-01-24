1d4b0bded029ac31c9b0e05b64c0dd2f3fcb3ccd
wendykuo-stemcell <wendy.kuo@stemcell.com>
2024-01-24 10:20:03 -0800

    added react grid layout

--- DailyTasksApp/src/App.jsx
+++ DailyTasksApp/src/App.jsx
@@ -1,21 +1,22 @@
 import { useState } from 'react'
-import { Responsive, WidthProvider } from "react-grid-layout";
+import GridLayout from "react-grid-layout";
 import './App.css'
 import data from './assets.json'
 import List from './components/List'
 import Header from './components/Header'
 import SideBar from './components/SideBar'
-const ResponsiveGridLayout = WidthProvider(Responsive);
+// const ResponsiveGridLayout = WidthProvider(Responsive);
 
 function App() {
   const layout = [
-    { i: "a", x: 0, y: 0, w: 1, h: 2},
-    { i: "b", x: 1, y: 0, w: 1, h: 2},
-    { i: "c", x: 2, y: 0, w: 1, h: 2 },
-    { i: "d", x: 3, y: 0, w: 1, h: 2 },
-    { i: "e", x: 4, y: 0, w: 1, h: 2 },
-    { i: "f", x: 5, y: 0, w: 1, h: 2 },
-    { i: "g", x: 6, y: 0, w: 1, h: 2 }
+    { i: "a", x: 0, y: 0, w: 1, h: 1},
+    { i: "b", x: 1, y: 0, w: 1, h: 1},
+    { i: "c", x: 2, y: 6, w: 3, h: 1 },
+    { i: "d", x: 3, y: 0, w: 1, h: 1 },
+    { i: "e", x: 4, y: 0, w: 1, h: 1 },
+    { i: "f", x: 5, y: 0, w: 1, h: 1 },
+    { i: "g", x: 6, y: 0, w: 1, h: 1 },
+    { i: "h", x: 3, y: 3, w: 1, h: 1 }
   ];
   return (
     <div className="app">
@@ -34,22 +35,25 @@ function App() {
             <p>Sunday</p>
 
           </div>
-          <ResponsiveGridLayout
+        
+          <GridLayout
             className="layout"
-            layout={layout}
+
             // breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-       
+            cols={12}
             rowHeight={30}
             width={1200}
           >
-            <div key="a">a</div>
-            <div key="b">b</div>
-            <div key="c">c</div>
-            <div key="d">d</div>
-            <div key="e">e</div>
-            <div key="f">f</div>
-            <div key="g">g</div>
-          </ResponsiveGridLayout>
+            <div key="a" data-grid={{x: 0, y: 0, w: 2, maxW: 3, h:1, static:true}}>a</div>
+            <div key="b" data-grid={{x: 1, y: 4, w: 3, maxW: 3, h:1, static:true}}>Saturday</div>
+            <div key="c" data-grid={{x: 2, y: 6, w: 1, maxW: 3, h:1, static:true}}>c</div>
+            <div key="d" data-grid={{x: 3, y: 6, w: 1, maxW: 3, h:1, static:true}}>d</div>
+            <div key="e" data-grid={{x: 4, y: 6, w: 1, maxW: 3, h:1, static:true}}>e</div>
+            <div key="f" data-grid={{x: 5, y: 2, w: 1, maxW: 3, h:1, static:true}}>f</div>
+            <div key="g" data-grid={{x: 3, y: 3, w: 1, maxW: 3, h:1, static:true}}>g</div>
+            <div key="h" data-grid={{x: 1, y: 6, w: 1, maxW: 3, h:1, static:true}}>h</div>
+            <div key="i" data-grid={{x: 0, y: 6, w: 1, maxW: 3, h:1, static:true}}>i</div>
+          </GridLayout>
         </div>
       </div>
 
